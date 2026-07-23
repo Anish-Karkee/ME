0fdcf304b54a4a6fe5a83f15ef5ae105d5c773ac
Anish-Karkee <anitakarkey7@gmail.com>
2026-07-23 21:06:13 +0545

    feat: just to check the push of image in the github repo.

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -140,7 +140,7 @@ export function HeroSection() {
                     fill 
                     priority
                     sizes="(max-width: 768px) 100vw, 400px"
-                    className="object-contain object-center group-hover:scale-105 transition-transform duration-700 ease-out" // just to check the push of image in the github repo.
+                    className="object-contain object-center group-hover:scale-105 transition-transform duration-700 ease-out" 
                   />
 
                   {/* Micro Badge inside image */}
